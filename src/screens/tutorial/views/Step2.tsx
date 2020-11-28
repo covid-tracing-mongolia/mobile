bed17f2d5081ea5b3b2989365422a7af5e057d6a
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-11-28 16:09:59 +0800

    feat(how-it-works): change illustrations

--- src/screens/tutorial/views/Step2.tsx
+++ src/screens/tutorial/views/Step2.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import {ItemView, ItemViewProps} from './ItemView';
 
 export const Step2 = (props: Pick<ItemViewProps, 'isActive'>) => {
-  return <ItemView {...props} item="step-2" image={require('assets/how-it-works-exposure.png')} />;
+  return <ItemView {...props} item="step-2" image={require('assets/how-it-works/exposure.png')} />;
 };
