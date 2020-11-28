28897a9f8c6ca12dbaf6cf5788687a6c51d7b7b2
khangaridb <khangarid.ba@gmail.com>
2020-11-28 17:53:15 +0800

    fixed ci

--- src/screens/testScreen/views/Item.tsx
+++ src/screens/testScreen/views/Item.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {useTheme} from '@shopify/restyle';
 import {Box, Ripple, Text} from 'components';
-import {Theme} from 'shared/theme';
 
 export interface ItemProps {
   title: string;
@@ -11,10 +9,9 @@ export interface ItemProps {
 }
 
 export const Item = ({title, subtitle, onPress, connectedRight}: ItemProps) => {
-  const theme = useTheme<Theme>();
   const connectedRightElement =
     typeof connectedRight === 'string' ? (
-      <Text color="overlayBodyText" {...theme.textVariants.menuItemTitle}>
+      <Text color="darkText" variant="menuItemText">
         {connectedRight}
       </Text>
     ) : (
@@ -24,14 +21,10 @@ export const Item = ({title, subtitle, onPress, connectedRight}: ItemProps) => {
   const content = (
     <Box flexDirection="row" height={48} alignItems="center" paddingHorizontal="m">
       <Box flex={1}>
-        <Text color="overlayBodyText" {...theme.textVariants.menuItemTitle}>
+        <Text variant="menuItemText" color="darkText">
           {title}
         </Text>
-        {subtitle && (
-          <Text color="bodyTextSubdued" {...theme.textVariants.menuItemSubtitle}>
-            {subtitle}
-          </Text>
-        )}
+        {subtitle && <Text color="bodyTextSubdued">{subtitle}</Text>}
       </Box>
       {connectedRightElement}
     </Box>
